3be082106e09fa6f43cb819d9321b0b6021dbb34
tt <sydney118227@icloud.com>
2023-05-02 13:40:19 +0800

    update download table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -109,8 +109,10 @@ function App() {
             res["Predict Result"] = r["pred"];
             res[`up payout`] = r["data"][0]["up_payout"];
             res[`down payout`] = r["data"][0]["down_payout"];
-            res[`PREV UP Payout`] = r["data"][2]["up_payout"];
-            res[`PREV DOWN Payout`] = r["data"][2]["down_payout"];
+            res[`PREV UP Payout`] =
+              index > 1 ? results[index - 2]["data"][2]["up_payout"] : "";
+            res[`PREV DOWN Payout`] =
+              index > 1 ? results[index - 2]["data"][2]["down_payout"] : "";
 
             // r.map((g, i) => {
             //   res["dateTime"] = blive[index][0]["datetime"];
